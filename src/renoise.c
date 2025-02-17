a4153f163cf35406992cbe22c62e5eca216165ee
gstaaij <gstaaij@protonmail.com>
2025-02-17 15:29:05 +0100

    Fix 2PI bug not being 2*PI, but 2/PI

--- src/renoise.c
+++ src/renoise.c
@@ -2,7 +2,7 @@
 #include <math.h>
 
 Renoise_Gradient_Point renoise_gradient_point_generate() {
-    double angle = ((double) random() / 2147483647.0) * M_2_PI;
+    double angle = ((double) random() / 2147483647.0) * 2*M_PI;
     return(Renoise_Gradient_Point) {
         .x = cos(angle),
         .y = sin(angle),
