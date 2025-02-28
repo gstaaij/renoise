89ee0b61aac7f052bb8b0fbf255838392710a648
gstaaij <gstaaij@protonmail.com>
2025-02-28 11:11:31 +0100

    Add basic gradient visualization

--- example/object_impermanence.c
+++ example/object_impermanence.c
@@ -8,19 +8,29 @@ int main() {
     
     Renoise_Chunk chunk = renoise_chunk_generate(3, 6, 0.2);
     uint64_t grad_point_count = chunk.grad_point_count_x * chunk.grad_point_count_y;
-    for (uint64_t i = 0; i < grad_point_count; ++i) {
-        uint64_t x = i % chunk.grad_point_count_x;
-        uint64_t y = i / chunk.grad_point_count_y;
-        printf("%"PRIu64", %"PRIu64":  {%lf; %lf}\n", x, y, chunk.grad_points[i].x, chunk.grad_points[i].y);
-    }
-
-    return 0;
 
     InitWindow(800, 600, "Hello, Raylib!");
 
     while (!WindowShouldClose()) {
         BeginDrawing();
-        ClearBackground(RED);
+            ClearBackground(BLACK);
+            for (uint64_t i = 0; i < grad_point_count; ++i) {
+                uint64_t x = i % chunk.grad_point_count_x;
+                uint64_t y = i / chunk.grad_point_count_y;
+                #define SCALE 200
+                int xpos = x * SCALE + SCALE/2;
+                int ypos = y * SCALE + SCALE/2;
+                DrawRectangle(xpos - SCALE/20, ypos - SCALE/20, SCALE/10, SCALE/10, WHITE);
+                DrawLineEx(
+                    (Vector2) { xpos, ypos },
+                    (Vector2) {
+                        xpos + chunk.grad_points[i].x * SCALE/2,
+                        ypos + chunk.grad_points[i].y * SCALE/2,
+                    },
+                    SCALE/20.0,
+                    RED
+                );
+            }
         EndDrawing();
     }
 
